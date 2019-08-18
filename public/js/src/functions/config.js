a3f6bdc7d1fab7834c141ad5fb4b4f9e70469b60
Byron Rocha <byronrocha97@hotmail.com>
2019-08-17 20:40:29 -0600

    .

--- public/js/src/functions/config.js
+++ public/js/src/functions/config.js
@@ -1,10 +1,10 @@
 const AppConfig = {
   BRANDNAME: process.env.BRANDNAME || '(K)-UCA',
   PORT:process.env.PORT ||8080,
-  HOST: process.env.HOST || "https://localhost:3000",
+  HOST: process.env.HOST || "http://ec2-18-205-27-185.compute-1.amazonaws.com",
   API: process.env.API || "https://main20190810065704.azurewebsites.net/api/",
-  MAIL: process.env.MAIL_PASSWORD || "correo@correo.com",
-  MAIL_PASSWORD: process.env.MAIL_PASSWORD || "password",
+  MAIL: process.env.MAIL_PASSWORD || "byronrochaxxx@gmail.com",
+  MAIL_PASSWORD: process.env.MAIL_PASSWORD || "Managua1997",
   SESSION_SECRET_LETTER: process.env.SESSION_SECRET_LETTER || "secret-session-001",
 }
 
